9372a4a0261c8a1c45266da9b56b296a3a15191e
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2022-01-26 15:35:45 -0500

    Add basic login

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+import { Client, Intents } from "discord.js"
+
 console.log('Hello world!')
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.once('ready', async () => {
+    console.log(`Logged in as ${client.user?.tag}`)
+})
+
+client.on('error', error => {
+    console.error('ERROR: ', error)
+})
+
+client.login(process.env.TOKEN!)
